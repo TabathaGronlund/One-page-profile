7f072e483ad405ead5d2ff4e0adfaca4e62c1d99
TabathaGronlund <tabathagronlund21@gmail.com>
2023-03-27 21:04:20 -0500

    adding HTML,CSS, and JS file

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+function changeUser() {
+    var c= document.getElementById("notme");
+    c.innerText = "ahtabaT dnulnorG";
+}
+
+function remove(element){
+    element.parentNode.parentNode.remove();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var plus = 500+
+// var addFriends = document.getElementById("addcon")
+
+// function addcon() {
+//     plus++;
+//     addFriends.innerText = plus;
+//     console.log (plus);
+// }
